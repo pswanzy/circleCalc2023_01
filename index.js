5ca7b9100448002c0fb424e13ab45b733c69ebf4
Peter Swanzy <120614923+pswanzy@users.noreply.github.com>
2023-05-08 17:56:46 -1000

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,19 @@ app.get("/", function(req, res) {
 // this gets the response from the values in the web page
 app.post("/", function(req, res){
 
-  var radCircle = parseFloat(req.body.n1);;
+  var radCircle = parseFloat(req.body.radius);
+  var cylHeight = parseFloat(req.body.height);
 
 // does the computation of the input variables, as numbers
     var areaCircle = Math.PI * Math.pow(radCircle, 2);
-    var circumCircle = 2 * Math.PI * radCircle;
+    var volCyl = height * areaCircle;
 
 // sends the results back to the web page as string
-  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
+  res.send("Volume of the cylinder is  " + volCyl.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
 // COMMENT out this code in Repl, as this is not running on localhost:3000
 
-app.listen(3000, function() {
-console.log ("Server is running on port 3000")
+// app.listen(3000, function() {
+// console.log ("Server is running on port 3000")
 });
